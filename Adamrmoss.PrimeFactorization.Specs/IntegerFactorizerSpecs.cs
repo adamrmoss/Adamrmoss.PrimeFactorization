85ec440b62a0440097cac8542ca9e154416c554f
Adam R Moss <adamrmoss@gmail.com>
2017-11-15 14:43:55 -0500

    Stub out IntegerFactorizer
    We also provide some failing specs.

---
+++ Adamrmoss.PrimeFactorization.Specs/IntegerFactorizerSpecs.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Adamrmoss.PrimeFactorization.Specs
+{
+    [TestFixture]
+    public class IntegerFactorizerSpecs
+    {
+        private IntegerFactorizer integerFactorizer;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.integerFactorizer = new IntegerFactorizer();
+        }
+
+        [Test]
+        public void WhenIFactorizeAPrimeNumber_ThenIReturnOnlyThatNumber()
+        {
+            var result = this.integerFactorizer.GetPrimeFactors(11);
+
+            result.ShouldBeEquivalentTo(new[] { 11 });
+        }
+
+        [Test]
+        public void WhenIFactorizeTheNegationOfAPrimeNumber_ThenIReturnNegativeOneAndThatNumber()
+        {
+            var result = this.integerFactorizer.GetPrimeFactors(11);
+
+            result.ShouldBeEquivalentTo(new[] { -1, 11 });
+        }
+
+        [Test]
+        public void WhenIFactorizeACompositeNumberWithNoRepeatedFactors_ThenIReturnAllFactors()
+        {
+            var result = this.integerFactorizer.GetPrimeFactors(30);
+
+            result.ShouldBeEquivalentTo(new[] { 2, 3, 5 });
+        }
+
+        [Test]
+        public void WhenIFactorizeACompositeNumberWithRepeatedFactors_ThenIReturnAllFactors()
+        {
+            var result = this.integerFactorizer.GetPrimeFactors(384);
+
+            result.ShouldBeEquivalentTo(new[] { 2, 2, 2, 2, 2, 2, 2, 3 });
+        }
+    }
+}
---
+++ Adamrmoss.PrimeFactorization/IntegerFactorizer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Adamrmoss.PrimeFactorization
+{
+    public class IntegerFactorizer
+    {
+        public IReadOnlyList<int> GetPrimeFactors(int intToFactorize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
