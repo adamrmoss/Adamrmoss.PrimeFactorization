67692efaea89154a95cc1a4356fc9329892ce6bc
Adam R Moss <adamrmoss@gmail.com>
2017-11-15 14:01:15 -0500

    Create IntegerLoader, associated (failing) tests w/Fixture Files

---
+++ Adamrmoss.PrimeFactorization.Specs/IntegerLoader/IntegerLoaderSpecs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Adamrmoss.PrimeFactorization.Specs
+{
+    [TestFixture]
+    public class IntegerLoaderSpecs
+    {
+        private IntegerLoader integerLoader;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.integerLoader = new IntegerLoader();
+        }
+
+        [Test]
+        public void WhenILoadNonExistantFile_ThenIFail()
+        {
+            Action loadingFileAction = () => this.integerLoader.GetIntegersFromFile("Fixtures/NotAnActualFile.txt");
+
+            loadingFileAction.ShouldThrow<FileNotFoundException>();
+        }
+
+        [Test]
+        public void WhenILoadInvalidFile_ThenIFail()
+        {
+            Action loadingFileAction = () => this.integerLoader.GetIntegersFromFile("Fixtures/InvalidInput.txt");
+
+            loadingFileAction.ShouldThrow<FormatException>();
+        }
+
+        [Test]
+        public void WhenILoadValidFile_ThenIDontFail()
+        {
+            Action loadingFileAction = () => this.integerLoader.GetIntegersFromFile("Fixtures/ValidInput.txt");
+
+            loadingFileAction.ShouldNotThrow<Exception>();
+        }
+
+        [Test]
+        public void WhenILoadValidFile_ThenIGetAListOfIntegers()
+        {
+            var ints = this.integerLoader.GetIntegersFromFile("Fixtures/ValidInput.txt");
+
+            ints.Count.Should().BeGreaterThan(0);
+        }
+    }
+}
---
+++ Adamrmoss.PrimeFactorization/IntegerLoader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Adamrmoss.PrimeFactorization
+{
+    public class IntegerLoader
+    {
+        public IReadOnlyList<int> GetIntegersFromFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
